5b56e1b35283a630c7ddb22cc877e24c4268025e
Chang <changyook@gmail.com>
2019-06-14 23:03:44 -0700

    Completed Question 917

---
+++ src/ReverseOnlyLetters.java
@@ -0,0 +1,36 @@
+public class ReverseOnlyLetters {
+    /*
+    Given a string S, return the "reversed" string where all characters that are not a letter stay in the same place, and all letters reverse their positions.
+
+    Example 1:
+
+    Input: "ab-cd"
+    Output: "dc-ba"
+    Example 2:
+
+    Input: "a-bC-dEf-ghIj"
+    Output: "j-Ih-gfE-dCba"
+    Example 3:
+
+    Input: "Test1ng-Leet=code-Q!"
+    Output: "Qedo1ct-eeLg=ntse-T!"
+
+    */
+    public String reverseOnlyLetters(String S) {
+        StringBuilder builder = new StringBuilder("");
+        for (int i = 0; i < S.length(); i++) {
+            if (Character.isLetter(S.charAt(i))) {
+                builder.insert(0, S.charAt(i));
+            }
+        }
+        for (int i = 0; i < S.length(); i++) {
+            if (!Character.isLetter(S.charAt(i))) {
+                builder.insert(i, S.charAt(i));
+
+            }
+        }
+
+        return builder.toString();
+    }
+
+}
