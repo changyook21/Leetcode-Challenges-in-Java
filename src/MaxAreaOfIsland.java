65f8423cde135289fc02475e9e0327850bbc60f6
Chang <changyook@gmail.com>
2019-08-10 21:19:20 -0700

    Completed Question 695

---
+++ src/MaxAreaOfIsland.java
@@ -0,0 +1,45 @@
+public class MaxAreaOfIsland {
+    /*
+    Given a non-empty 2D array grid of 0's and 1's, an island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+
+    Find the maximum area of an island in the given 2D array. (If there is no island, the maximum area is 0.)
+
+    Example 1:
+
+    [[0,0,1,0,0,0,0,1,0,0,0,0,0],
+     [0,0,0,0,0,0,0,1,1,1,0,0,0],
+     [0,1,1,0,1,0,0,0,0,0,0,0,0],
+     [0,1,0,0,1,1,0,0,1,0,1,0,0],
+     [0,1,0,0,1,1,0,0,1,1,1,0,0],
+     [0,0,0,0,0,0,0,0,0,0,1,0,0],
+     [0,0,0,0,0,0,0,1,1,1,0,0,0],
+     [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+    */
+    public int maxAreaOfIsland(int[][] grid) {
+        int max = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    int cur = dfs(grid,i,j);
+                    if (cur > max) {
+                        max = cur;
+                    }
+                }
+            }
+        }
+        return max;
+    }
+
+    public int dfs (int[][] grid, int row, int col) {
+        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] == 0) {
+            return 0;
+
+        }
+
+        grid[row][col] = 0;
+        return (1 + dfs(grid, row - 1, col) + dfs(grid, row + 1, col) + dfs(grid, row, col - 1) + dfs(grid, row, col + 1));
+
+    }
+
+
+}
